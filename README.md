610957fc797607d302127685e757f1d4b3541186
tugi64 <turgay.tekdemir@gmail.com>
2025-11-27 22:25:47 +0300

    Initial Android project setup for TugisCAD

---
+++ README.md
@@ -0,0 +1,25 @@
+# TugisCAD
+
+Android tabanlı CAD (Computer-Aided Design) uygulaması.
+
+## Geliştirme
+
+### Gereksinimler
+- Android Studio (son sürüm önerilir)
+- JDK 11 veya üzeri
+- Android SDK (API Level 24+)
+
+### Kurulum
+1. Projeyi klonlayın
+```bash
+git clone git@github.com:tugi64/tugiscad.git
+```
+
+2. Android Studio ile açın
+3. Gradle sync işlemini tamamlayın
+4. Uygulamayı çalıştırın
+
+## Lisans
+
+[Lisans bilgisi eklenecek]
+
---
+++ app/src/main/java/com/tugi/tugiscad/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.tugi.tugiscad
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tugi.tugiscad.ui.theme.TugisCADTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TugisCADTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("TugisCAD")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Welcome to $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TugisCADTheme {
+        Greeting("TugisCAD")
+    }
+}
+
